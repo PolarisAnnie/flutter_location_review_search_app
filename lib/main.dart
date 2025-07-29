8f79d29f4719ec93f2844f7805f5ab3c969b23b0
최은희 <polarisinuniverse@choeeunhuiui-iMac.local>
2025-07-29 19:39:18 +0900

    initial settings

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:location_review_search_app/ui/pages/home/home_page.dart';
+
+void main() {
+  // ProviderScope 로 앱을 감싸서 RiverPod이 ViewModel 관리할 수 있게 선언
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: HomePage());
+  }
+}
---
+++ lib/ui/pages/review/review_page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ReviewPage extends StatelessWidget {
+  @override
+  Widget build(Object context) {
+    return Scaffold(appBar: AppBar(title: Text('ReviewPage')));
+  }
+}

a7997329c4c7b1f508b6e05e09a0a0666c0432e5
최은희 <polarisinuniverse@choeeunhuiui-iMac.local>
2025-07-29 20:31:33 +0900

    feat: HomePage UI 및 AddressItem 위젯 추가

--- lib/ui/pages/home/home_page.dart
+++ lib/ui/pages/home/home_page.dart
@@ -1,8 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:location_review_search_app/ui/pages/home/widgets/address_item.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   @override
-  Widget build(Object context) {
-    return Scaffold(appBar: AppBar(title: Text('HomePage')));
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  TextEditingController textEditingController = TextEditingController();
+
+  void search(String text) {
+    print('search');
+  }
+
+  @override
+  void dispose() {
+    textEditingController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: TextField(
+            maxLines: 1,
+            controller: textEditingController,
+            onSubmitted: search,
+            decoration: InputDecoration(
+              hintText: '검색어를 입력해주세요',
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.grey),
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: ListView(
+            children: [
+              SizedBox(height: 12),
+              AddressItem(
+                '삼성1동 주민센터',
+                '공공,사회기관>행정복지센터',
+                '서울특별시 강남구 봉은사로 616 삼성1동 주민센터',
+              ),
+              SizedBox(height: 12),
+              AddressItem(
+                '삼성2동 주민센터',
+                '공공,사회기관>행정복지센터',
+                '서울특별시 강남구 봉은사로 616 삼성2동 주민센터',
+              ),
+              SizedBox(height: 12),
+              AddressItem('코엑스', '문화,예술>컨벤션센터', '서울특별시 강남구 영동대로 513'),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/ui/pages/home/widgets/address_item.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:location_review_search_app/ui/pages/review/review_page.dart';
+
+class AddressItem extends StatelessWidget {
+  String title;
+  String category;
+  String roadAddress;
+
+  AddressItem(this.title, this.category, this.roadAddress);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              // TODO: ReviewPage에 타이틀, mapX, mapY 좌표 전달
+              return ReviewPage();
+            },
+          ),
+        );
+      },
+      child: Container(
+        height: 110,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(color: Colors.grey[300]!),
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(left: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                title,
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+              ),
+              Text(category, style: TextStyle(fontSize: 13)),
+              Text(roadAddress, style: TextStyle(fontSize: 13)),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
